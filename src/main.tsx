c1120ee02b474eab3fbbdebe855afdd732bb4e91
jesusmald.23@gmail.com <jesumald.23@gmail.com>
2023-06-27 12:41:35 -0500

    hola

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import LoginPage from "./pages/auth/login.page";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
     
-      <App />
+    <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
       <ToastContainer />
     </BrowserRouter>
   </React.StrictMode>
