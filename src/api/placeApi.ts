e1a22591f687a7e5ba6fba5887f4be2a1459be09
Jesusrm2 <135676762+Jesusrm2@users.noreply.github.com>
2024-03-21 11:55:59 -0500

    Update placeApi.ts

--- src/api/placeApi.ts
+++ src/api/placeApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const placeApi = axios.create({
     baseURL: '/maps/api/place/details',
     params:{
-    key: 'AIzaSyC4i2ej2NQxOwvw9hpGkTZVpADP8FYAuKk',
+    key: 'api_key',//api key google maps
     fields:'name,opening_hours,business_status,vicinity,formatted_phone_number'
     
 }
